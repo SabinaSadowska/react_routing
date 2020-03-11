add5774edc9980661deba3c4a1c2711b39aae4bc
SabinaSadowska <sadowwwska@gmail.com>
2020-03-11 14:54:34 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Switch, Route} from'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Default from './pages/default'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <BrowserRouter>    
+    <Switch>      
+        <Route exact path='/' component={Home} />      
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} /> 
+        <Route component={Default} /> 
+    </Switch>  
+</BrowserRouter>,  document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();

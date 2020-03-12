6bda38d5b49aec1dd4ca2ff421d704f2ebe5f573
SabinaSadowska <sadowwwska@gmail.com>
2020-03-12 14:17:32 +0100

    propTyper

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types'
 
 function Header(props) {
 return <div>
     <h1>{props.header}</h1>
 </div>
+}
+
+Header.propTypes = {
+    header: PropTypes.string
 }
 
 export default Header
